41aa1e0b01439761ed9551253858e01ef7a1542a
ChardonBleu <f.rondi@orange.fr>
2025-04-16 12:16:56 +0200

    ✨ feat: add fetch on photographers json

--- scripts/components/photographerCard.js
+++ scripts/components/photographerCard.js
@@ -1,15 +1,18 @@
 import constants from "../utils/constants.js";
 
 export default function photographerTemplate(data) {
-  const { name, portrait } = data;
+  const { name, city, country, tagline, price, portrait } = data;
 
   const picture = constants.portraitURL + `${portrait}`;
 
   const article = document.createElement("article");
 
   const userCard = `
-    <img src="${picture}" alt="${name} portrait"/>
+    <img src="${picture}" alt="${name} portrait" class="portrait"/>
     <h2>${name}</h2>
+    <p class="location">${city}, ${country}</p>
+    <p class="tag">${tagline}</p>
+    <p class="price">${price}€/jour</p>
   `;
   article.innerHTML = userCard;
 
