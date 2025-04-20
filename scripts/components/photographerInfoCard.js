01254c059dd4d9f73dd831f73ae1d9eef9b19855
ChardonBleu <f.rondi@orange.fr>
2025-04-20 16:28:02 +0200

    ✨ feat: add some accessibiliyt to photographer page

--- scripts/components/photographerInfoCard.js
+++ scripts/components/photographerInfoCard.js
@@ -4,13 +4,13 @@ export default function photographerInfoTemplate(data) {
   const picture = portraitURL + `${data.portrait}`;
 
   const artistInfo = `
-    <div class="artist-info">
+    <div class="artist-info" aria-label="Galerie du photographe ${data.name}">
       <h2>${data.name}</h2> 
       <p class="location" tabindex="0">${data.city}, ${data.country}</p>
       <p class="tag" tabindex="0">${data.tagline}</p>
     </div>
-    <button class="contact_button">Contactez-moi</button>
-    <div  class="portrait"><img src="${picture}" alt=""/></div>   
+    <button class="contact_button" aria-label="pour ouvrir un formulaire de contact">Contactez-moi</button>
+    <div  class="portrait" aria-hidden="true"><img src="${picture}" alt=""/></div>   
   `;
 
   return artistInfo;
