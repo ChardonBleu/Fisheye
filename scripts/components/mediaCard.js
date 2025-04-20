2c7f37d4a82d676dbb24b76283efe0e31fcc68ed
ChardonBleu <f.rondi@orange.fr>
2025-04-20 17:08:14 +0200

    ✨ feat: add aside total likes and price

--- scripts/components/mediaCard.js
+++ scripts/components/mediaCard.js
@@ -4,9 +4,9 @@ export default function mediaTemplate(media) {
   const artistInfo = `
     <div  class="original-media artist-media" aria-hidden="true">${media.mediaElement}</div>
 
-    <div class="media-info" aria-label="description de la photo">
-      <h2 tabindex="0">${media.title}</h2> 
-      <p class="likes" tabindex="0" aria-label="nombre de likes">${media.likes}</p>
+    <div class="media-info">
+      <h2 aria-label="photo ${media.title}" tabindex="0">${media.title}</h2> 
+      <p class="likes" tabindex="0" aria-label="likes de la photo">${media.likes}</p>
       <i class="heart fa-solid fa-heart" aria-hidden="true"></i>
     </div>
   `;
