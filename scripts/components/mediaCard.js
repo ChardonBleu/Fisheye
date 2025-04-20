0846f6d1941a006af4dd5a82fa8e556b280e0a0b
ChardonBleu <f.rondi@orange.fr>
2025-04-20 16:28:02 +0200

    ✨ feat: add some accessibiliyt to photographer page

--- scripts/components/mediaCard.js
+++ scripts/components/mediaCard.js
@@ -2,12 +2,12 @@ export default function mediaTemplate(media) {
   const article = document.createElement("article");
 
   const artistInfo = `
-    <div  class="original-media artist-media">${media.mediaElement}</div>
+    <div  class="original-media artist-media" aria-hidden="true">${media.mediaElement}</div>
 
-    <div class="media-info">
-      <h2>${media.title}</h2> 
-      <p class="likes" tabindex="0">${media.likes}</p>
-      <i class="heart fa-solid fa-heart"></i>
+    <div class="media-info" aria-label="description de la photo">
+      <h2 tabindex="0">${media.title}</h2> 
+      <p class="likes" tabindex="0" aria-label="nombre de likes">${media.likes}</p>
+      <i class="heart fa-solid fa-heart" aria-hidden="true"></i>
     </div>
   `;
 
